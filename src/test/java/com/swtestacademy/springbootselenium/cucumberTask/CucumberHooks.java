0b014d003ebdfd13d51792d5d6486bf7a1e2707a
SenolBulut <senol.bulut@roofstacks.com>
2022-03-28 22:13:26 +0300

    added new feature

--- src/test/java/com/swtestacademy/springbootselenium/cucumberTask/CucumberHooks.java
+++ src/test/java/com/swtestacademy/springbootselenium/cucumberTask/CucumberHooks.java
@@ -3,11 +3,12 @@ package com.swtestacademy.springbootselenium.cucumberTask;
 import com.swtestacademy.springbootselenium.annotations.LazyAutowired;
 import com.swtestacademy.springbootselenium.utils.ScreenshotUtil;
 import io.cucumber.java.*;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.springframework.context.ApplicationContext;
 
 import java.time.Duration;
-
+@Slf4j
 public class CucumberHooks {
 
     @LazyAutowired
@@ -24,7 +25,7 @@ public class CucumberHooks {
         driver.manage().window().maximize();
         driver.manage().timeouts().getPageLoadTimeout();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        System.out.println("         ---->"+scenario.getName()+"<----");
+        log.info("         ---->"+scenario.getName()+"<----");
     }
 
     @AfterStep
